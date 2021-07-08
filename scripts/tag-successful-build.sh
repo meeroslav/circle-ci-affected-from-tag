2abf2aeef959a8a639b1de46ad8b5ef7bca28b55
Miroslav Jonas <missing.manual@gmail.com>
2021-07-08 10:37:25 +0200

    feat: push single tag

--- scripts/tag-successful-build.sh
+++ scripts/tag-successful-build.sh
@@ -5,5 +5,6 @@ INPUTS_TAG_MATCH_PATTERN=$2
 
 # remove all tags first
 git push -delete origin $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
+
 git tag $command_string_to_echo_as_tag_name
-git push origin --tags
+git push origin $command_string_to_echo_as_tag_name
