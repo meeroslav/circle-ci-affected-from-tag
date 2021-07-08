6870d6b871adc2709bad6ed9100c36f2f2f61bd7
Miroslav Jonas <missing.manual@gmail.com>
2021-07-08 10:30:54 +0200

    feat: remove all previous tags on remote

--- scripts/tag-successful-build.sh
+++ scripts/tag-successful-build.sh
@@ -4,6 +4,6 @@ command_string_to_echo_as_tag_name=$1
 INPUTS_TAG_MATCH_PATTERN=$2
 
 # remove all tags first
-git tag -d $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
+git push -delete origin $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
 git tag $command_string_to_echo_as_tag_name
 git push origin --tags
