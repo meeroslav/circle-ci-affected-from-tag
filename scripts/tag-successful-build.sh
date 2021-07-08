feafbab0cfa1b0a8347ea4ae0744a2e4130b4770
Miroslav Jonas <missing.manual@gmail.com>
2021-07-08 10:54:14 +0200

    fix: add command

--- scripts/tag-successful-build.sh
+++ scripts/tag-successful-build.sh
@@ -5,6 +5,6 @@ INPUTS_TAG_MATCH_PATTERN=$2
 
 # remove all tags first
 git push -d origin $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
-
+# set single tag
 git tag $command_string_to_echo_as_tag_name
 git push origin $command_string_to_echo_as_tag_name
