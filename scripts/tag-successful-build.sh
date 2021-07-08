bc4f6a0f09f1cde6ca2d2d78b1d781f20a3bc109
Miroslav Jonas <missing.manual@gmail.com>
2021-07-08 10:37:55 +0200

    fix: delete command

--- scripts/tag-successful-build.sh
+++ scripts/tag-successful-build.sh
@@ -4,7 +4,7 @@ command_string_to_echo_as_tag_name=$1
 INPUTS_TAG_MATCH_PATTERN=$2
 
 # remove all tags first
-git push -delete origin $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
+git push -d origin $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
 
 git tag $command_string_to_echo_as_tag_name
 git push origin $command_string_to_echo_as_tag_name
