086efc8344fee20b8d5f1cb9cb28b08fca19a484
Miroslav Jonas <missing.manual@gmail.com>
2021-07-08 10:19:34 +0200

    feat: remove all previous tags

--- scripts/tag-successful-build.sh
+++ scripts/tag-successful-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 command_string_to_echo_as_tag_name=$1
+INPUTS_TAG_MATCH_PATTERN=$2
 
+# remove all tags first
+git tag -d $(git tag -l "$INPUTS_TAG_MATCH_PATTERN")
 git tag $command_string_to_echo_as_tag_name
 git push origin --tags
