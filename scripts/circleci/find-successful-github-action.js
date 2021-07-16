f1f7bed236d3a29383c5001a51842a19bf133d83
Miroslav Jonas <missing.manual@gmail.com>
2021-07-16 10:53:46 +0200

    chore(repo): read params in js

---
+++ scripts/circleci/find-successful-github-action.js
@@ -0,0 +1,7 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const GITHUB_TOKEN = process.argv[2];
+
+// const octokit = github.getOctokit(core.getInput("github_token"));
+console.log(GITHUB_TOKEN);
